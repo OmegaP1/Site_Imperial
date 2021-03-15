df9068601e423942be6d8a151dbf186f526efda6
OmegaP1 <j.veloso.00@gmail.com>
2021-03-15 21:24:03 +0000

    Commit

--- server.js
+++ server.js
@@ -8,50 +8,76 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
 
-
-
 mongoose.connect(
-    "mongodb+srv://Nba_Tips:Nba_Tips@clusternba.decuk.mongodb.net/imperial?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (!err) {
-        console.log("MongoDB Connection Succeeded.");
-      } else {
-        console.log("Error in DB connection : " + err);
+  "mongodb+srv://Nba_Tips:Nba_Tips@clusternba.decuk.mongodb.net/imperial?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection : " + err);
+    }
+  }
+);
+
+numberOfRegioes = 0;
+Regioes = [];
+
+var wineSchema = new mongoose.Schema({
+  Nome: String,
+  Casta: String,
+  Teor_Alcoolico: String,
+  Preco: String,
+  Ano: int32,
+  Categoria: String,
+  Região: String,
+  Descricao1: String,
+  Descricao2: String,
+});
+
+const Vinhos = mongoose.model("vinhos", wineSchema);
+
+app.get("/", async function (req, res) {
+  await Vinhos.find({}, function (err, vinhos) {
+    res.render("Main_Menu.ejs");
+  });
+});
+
+app.get("/Vinhos", async function (req, res) {
+  Regioes = [];
+  await Vinhos.find({}, function (err, vinhos) {
+    for (i = 0; i < vinhos.length; i++) {
+      helper = vinhos[i].Região;
+      if (Regioes.length == 0) {
+        Regioes.push(helper);
+      }
+      if (
+        helper != Regioes[Regioes.length] &&
+        helper != Regioes[Regioes.length - 1]
+      ) {
+        Regioes.push(helper);
       }
     }
-  );
+    numberOfRegioes = Regioes.length;
+    console.log(Regioes);
 
-  var wineSchema = new mongoose.Schema({
-    Nome: String,
-    Casta: String,
-    Teor_Alcoolico: String,
-    Preco: String,
-    Ano: int32,
-    Descricao1 : String,
-    Descricao2 : String,
-  });
+    res.render("Vinhos.ejs", {
+      VinhosList: vinhos,
+    });
+  }).sort({ Região: 1 });
+});
 
+app.post("/Vinhos", async function (req, res) {
+  Name = req.body.NomeVinho;
+  console.log(Name);
+  findable = { Nome: Name };
+  await Vinhos.find({}, function (err, vinhos) {
+    res.render("DisplayVinhos.ejs", {
+      VinhosList: vinhos,
+    });
+  }).find(findable);
+});
 
-  const Vinhos = mongoose.model("vinhos", wineSchema);
-
-
-
-
-  app.get("/", async function (req, res) {
-    await Vinhos.find({}, function (err, vinhos) {
-      res.render("Vinhos.ejs", {
-        VinhosList: vinhos,
-      });
-    }).sort({ Nome: 1 });
-  });
-
-
-  
-
-  
-
-
-  app.listen(3000, function () {
-    console.log("Server is running in 3000");
-  });  
+app.listen(3000, function () {
+  console.log("Server is running in 3000");
+});
